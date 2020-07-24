d3474ff90c08529b8c875d3ad108012db106f807
Jan Schankin <jas@treeno.de>
2020-07-24 16:43:31 +0200

    Added a test for methods with parameters and String return values

--- core/src/test/java/org/deepmock/core/api/BehaviorsTest.java
+++ core/src/test/java/org/deepmock/core/api/BehaviorsTest.java
@@ -2,11 +2,16 @@ package org.deepmock.core.api;
 
 import org.deepmock.core.model.Behavior;
 import org.deepmock.core.model.BehaviorRepository;
+import org.deepmock.core.model.ParameterMatcher;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BehaviorsTest {
+public class BehaviorsTest {
+
+    public static final String PARAMETER_VALUE = "Blubb";
 
     @Test
     public void testBehaviorDefinitionWithoutParam() {
@@ -19,5 +24,26 @@ class BehaviorsTest {
         assertEquals(Quantity.class, currentBehavior.getJoinPoint().getTarget());
         assertTrue(currentBehavior.getParameter().isEmpty());
         assertEquals(4, currentBehavior.getReturnValueSupplier().supply());
+    }
+
+    @Test
+    public void testBehaviorDefinitionWithParam() {
+        //GIVEN WHEN
+        TestObject testObjectBehavior = Behaviors.of(TestObject.class);
+        Personality.hasTrait(testObjectBehavior.doSomeThing(PARAMETER_VALUE)).returning("New Behavior");
+
+        //THEN
+        Behavior currentBehavior = BehaviorRepository.getInstance().getCurrentBehavior();
+        List<ParameterMatcher> parameter = currentBehavior.getParameter();
+
+        assertEquals(parameter.size(), 1);
+        assertTrue(parameter.get(0).matches(PARAMETER_VALUE));
+    }
+
+    public static class TestObject {
+
+        public String doSomeThing(String someParameter) {
+            return someParameter;
+        }
     }
 }
