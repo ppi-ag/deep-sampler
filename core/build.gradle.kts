1c52e48f1347b51a2f6f6f6ae689ca0d00100eb1
Jan Schankin <jas@treeno.de>
2020-07-27 20:09:39 +0200

    +dependencies for generic .equals() and hashcode()

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation(group = "cglib", name = "cglib", version = "3.3.0")
     implementation(group = "cglib", name = "cglib", version = "3.3.0")
+    implementation(group = "org.apache.commons", name = "commons-lang3", version = "3.11")
 }
