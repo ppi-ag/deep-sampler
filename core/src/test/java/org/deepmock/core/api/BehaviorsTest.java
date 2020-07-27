ea315bfe5775bd37b3571cf245bb8d8feec83603
Jan Schankin <jas@treeno.de>
2020-07-27 20:09:02 +0200

    +tests

--- core/src/test/java/org/deepmock/core/api/BehaviorsTest.java
+++ core/src/test/java/org/deepmock/core/api/BehaviorsTest.java
@@ -1,5 +1,7 @@
 package org.deepmock.core.api;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.deepmock.core.model.Behavior;
 import org.deepmock.core.model.BehaviorRepository;
 import org.deepmock.core.model.ParameterMatcher;
@@ -12,6 +14,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class BehaviorsTest {
 
     public static final String PARAMETER_VALUE = "Blubb";
+    private static final Bean BEAN_A = new Bean("a", 1);
+    private static final Bean BEAN_A_COPY = new Bean("a", 1);
+    private static final Bean BEAN_B = new Bean("b", 2);
 
     @Test
     public void testBehaviorDefinitionWithoutParam() {
@@ -27,7 +32,7 @@ public class BehaviorsTest {
     }
 
     @Test
-    public void testBehaviorDefinitionWithParam() {
+    public void testBehaviorDefinitionWithPrimitiveParam() {
         //GIVEN WHEN
         TestObject testObjectBehavior = Behaviors.of(TestObject.class);
         Personality.hasTrait(testObjectBehavior.doSomeThing(PARAMETER_VALUE)).returning("New Behavior");
@@ -40,10 +45,49 @@ public class BehaviorsTest {
         assertTrue(parameter.get(0).matches(PARAMETER_VALUE));
     }
 
+    @Test
+    public void testBehaviorDefinitionWithBeanParam() {
+        //GIVEN WHEN
+        TestObject testObjectBehavior = Behaviors.of(TestObject.class);
+        Personality.hasTrait(testObjectBehavior.doSomeThing(BEAN_A)).returning(BEAN_B);
+
+        //THEN
+        Behavior currentBehavior = BehaviorRepository.getInstance().getCurrentBehavior();
+        List<ParameterMatcher> parameter = currentBehavior.getParameter();
+
+        assertEquals(parameter.size(), 1);
+        assertTrue(parameter.get(0).matches(BEAN_A_COPY));
+    }
+
     public static class TestObject {
 
         public String doSomeThing(String someParameter) {
             return someParameter;
+        }
+
+        public Bean doSomeThing(Bean bean) {
+            return bean;
+        }
+    }
+
+
+    public static class Bean {
+        private String someString;
+        private int someInt;
+
+        public Bean(String someString, int someInt) {
+            this.someString = someString;
+            this.someInt = someInt;
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            return EqualsBuilder.reflectionEquals(this, other);
+        }
+
+        @Override
+        public int hashCode() {
+            return HashCodeBuilder.reflectionHashCode(this);
         }
     }
 }
