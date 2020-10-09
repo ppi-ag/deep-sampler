9e1a66ebb1f97887d8f092311c52a4abc3db0f94
Jan Schankin <jas@treeno.de>
2020-10-09 19:20:19 +0200

    A new Extension for JUnit4

--- junit5/src/test/java/org/deepsampler/junit5/DeepSamplerExtensionTest.java
+++ junit5/src/test/java/org/deepsampler/junit5/DeepSamplerExtensionTest.java
@@ -3,13 +3,17 @@ package org.deepsampler.junit5;
 import org.deepsampler.core.api.Sample;
 import org.deepsampler.core.internal.ProxyFactory;
 import org.deepsampler.core.model.SampleRepository;
+import org.deepsampler.junit.PrepareSampler;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(DeepSamplerExtension.class)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class DeepSamplerExtensionTest {
 
     @PrepareSampler
@@ -25,6 +29,7 @@ public class DeepSamplerExtensionTest {
     @Test
     @Order(2)
     public void addSamplesThatShouldBeClearedInNextTest() {
+        assertTrue(SampleRepository.getInstance().isEmpty());
         Sample.of(testBeanSampler.getSomeInt()).is(1);
         assertFalse(SampleRepository.getInstance().isEmpty());
     }
