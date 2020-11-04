be95740f73d867db16e7de6eab8c9d70c15db73e
Jan Schankin <jas@treeno.de>
2020-11-04 18:32:18 +0100

    Moved test utilities into a test-fixture in order to remove test code from implementation code

--- provider/build.gradle.kts
+++ provider/build.gradle.kts
@@ -1,6 +1,14 @@
-dependencies {
-    implementation(project(":core"))
-    implementation(project(":persistence"))
-    implementation(project(":persistence-json"))
-    implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+plugins {
+    `java-test-fixtures`
 }
+
+dependencies {
+    testFixturesImplementation(project(":core"))
+    testFixturesImplementation(project(":persistence"))
+    testFixturesImplementation(project(":persistence-json"))
+    testFixturesImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testFixturesApi("org.apache.commons:commons-lang3:3.9")
+    testFixturesImplementation("org.apache.commons:commons-text:1.6")
+}
+
+
