5dcea01b137d2f1093482d135420c879fd0286ee
JanSchankin <jas@treeno.de>
2020-12-12 18:51:40 +0100

    Some additional tests to improve coverage

---
+++ deepsampler-provider/src/testFixtures/java/de/ppi/deepsampler/provider/common/TestBeanWithoutEquals.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2020  PPI AG (Hamburg, Germany)
+ * This program is made available under the terms of the MIT License.
+ */
+
+package de.ppi.deepsampler.provider.common;
+
+
+/**
+ * A very simple Bean that is used to test the behavior of {@link de.ppi.deepsampler.core.api.Matchers.EqualsMatcher} in
+ * situations where a parameter does not provide an equals()-method. Therefore this Bean does not have an equals() and is essentially
+ * empty.
+ */
+public class TestBeanWithoutEquals {
+
+
+}
