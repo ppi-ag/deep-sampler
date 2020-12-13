ea8d58b0735ee0b3a72d966314d21d0edde9504f
JanSchankin <jas@treeno.de>
2020-12-13 15:16:19 +0100

    Documentation

--- deepsampler-provider-spring/src/main/java/de/ppi/deepsampler/provider/spring/SpringSamplerAspect.java
+++ deepsampler-provider-spring/src/main/java/de/ppi/deepsampler/provider/spring/SpringSamplerAspect.java
@@ -14,11 +14,11 @@ import org.aspectj.lang.reflect.MethodSignature;
 import java.util.Arrays;
 
 /**
- * The SamplerInterceptor is responsible for the actual stubbing. It intercepts methods and decides which methods should be stubbed
- * using the SampleDefinitions.
+ * The {@link SpringSamplerAspect} is responsible for the actual stubbing. It intercepts methods and decides which methods should be stubbed
+ * using the {@link SampleDefinition}s.
  * <br>
  * Subclasses must be annotated with {@link org.aspectj.lang.annotation.Aspect} in order to tell Spring that this class is an Aspect.
- * Additionally this class must be added to a Spring-Config and the SpringConfig itself must enable AspectJProxies using the annotation
+ * Additionally this class must be added to a SpringConfig and the SpringConfig itself must enable AspectJProxies using the annotation
  * {@link org.springframework.context.annotation.EnableAspectJAutoProxy}.
  * <br>
  * The intercepted (i.e. stubbed) classes are defined using an AspectJ PointCut. This is done
@@ -34,8 +34,8 @@ public abstract class SpringSamplerAspect {
      * This method is used to define the classes that will be intercepted. This is done using the annotation
      * {@link org.aspectj.lang.annotation.Pointcut}.
      * <br>
-     * The organization of Pointcuts can be done by defining Pointcuts for layers, or modules of an application. Each Pointcut can
-     * be defined using a method that is named according to modules or layers. For instance:
+     * The organization of Pointcuts can be done by defining separate Pointcuts for layers, or modules of an application. Each Pointcut can
+     * be defined using a method that is named according to a module or a layer. For instance:
      * <br>
      * <pre>
      * @Pointcut("within(org.my.application.dao..*)")
@@ -58,16 +58,16 @@ public abstract class SpringSamplerAspect {
      * @param joinPoint The AOP-JoinPoint that describes which method will be called and which method can now be intercepted. I.e. this method
      *                  will become a stub if the method is described by a Sampler.
      * @return If the intercepted method is a stub, the return value is determined by the Sampler, otherwise this is the original return value coming from
-     * the original method as described by the joinPoint.
-     * @throws Throwable Since we intercept numerous methods in general it is possible that any kind of {@link Exception}, even {@link Throwable} is thrown.
+     * the original method as described by the {@code joinPoint}.
+     * @throws Throwable Since we intercept numerous methods in general, it is possible that any kind of {@link Exception}, even {@link Throwable} is thrown.
      * Even though declaring {@link Throwable} in a throws clause is usually not recommended, this is done by Spring itself (for comprehensible reasons),
      * so we are also forced to do so.
      */
-    @Around("!@within(org.springframework.context.annotation.Configuration) "
-            + "&& !@within(org.aspectj.lang.annotation.Aspect) "
-            + "&& !within(is(EnumType)) "
-            + "&& !within(is(FinalType)) "
-            + "&& include()")
+    @Around("!@within(org.springframework.context.annotation.Configuration) " // excludes all SpringConfigs
+            + "&& !@within(org.aspectj.lang.annotation.Aspect) " // Excludes all Aspects by excluding classes annotated with @Aspect
+            + "&& !within(is(EnumType)) " // Exludes all Enums
+            + "&& !within(is(FinalType)) " // Exludes all final classes
+            + "&& include()") // Delegates to the custom Pointcut that must be defined by overriding the Method SpringSamplerAspect::include
     public Object aroundMethod(final ProceedingJoinPoint joinPoint) throws Throwable {
         final SampleDefinition sampleDefinition = findSampleDefinition(joinPoint);
 
