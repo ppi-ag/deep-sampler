6e848b44e503a4655506149debc52ab6653b4378
JanSchankin <jas@treeno.de>
2021-01-15 17:39:01 +0100

    removed apache-commons to reduce the number of dependencies

--- deepsampler-core/build.gradle.kts
+++ deepsampler-core/build.gradle.kts
@@ -10,5 +10,4 @@ plugins {
 dependencies {
     implementation("org.javassist:javassist:3.27.0-GA")
     implementation("org.objenesis:objenesis:3.1")
-    implementation(group = "org.apache.commons", name = "commons-lang3", version = "3.11")
 }
