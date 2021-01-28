09346c2abb4327dba106d31f870359ebd228afbe
JanSchankin <jas@treeno.de>
2021-01-28 09:23:11 +0100

    +Another Test for Wrapper-Objects like Character

--- deepsampler-provider/src/testFixtures/java/de/ppi/deepsampler/provider/common/SamplerAspectTest.java
+++ deepsampler-provider/src/testFixtures/java/de/ppi/deepsampler/provider/common/SamplerAspectTest.java
@@ -523,9 +523,9 @@ public abstract class SamplerAspectTest {
         Sampler.clear();
 
         final TestService testServiceSampler = Sampler.prepare(TestService.class);
-        testServiceSampler.testSqlDate(new RecTestBean(new RecTestBean(null, "A"), "B"));
+        testServiceSampler.testSqlDate(new RecTestBean(new RecTestBean(null, "A", 'C'), "B", 'C'));
 
-        getTestService().testSqlDate(new RecTestBean(new RecTestBean(null, "A"), "B"));
+        getTestService().testSqlDate(new RecTestBean(new RecTestBean(null, "A", 'C'), "B", 'C'));
         final String pathToFile = "./record/sqlDateCanBeRecordedAndLoaded.json";
         final PersistentSampleManager source = PersistentSampler.source(JsonSourceManager.builder().buildWithFile(pathToFile));
         source.record();
@@ -534,13 +534,15 @@ public abstract class SamplerAspectTest {
         Sampler.clear();
         assertTrue(SampleRepository.getInstance().isEmpty());
 
-        testServiceSampler.testSqlDate(new RecTestBean(new RecTestBean(null, "A"), "B"));
+        testServiceSampler.testSqlDate(new RecTestBean(new RecTestBean(null, "A", 'C'), "B", 'C'));
         source.load();
 
         assertFalse(SampleRepository.getInstance().isEmpty());
-        assertEquals(new Date(1), getTestService().testSqlDate(new RecTestBean(new RecTestBean(null, "A"), "B")));
+        assertEquals(new Date(1), getTestService().testSqlDate(new RecTestBean(new RecTestBean(null, "A", 'C'), "B", 'C')));
         Files.delete(Paths.get(pathToFile));
     }
+
+
 
     @Test
     public void manualIdSetForRecordingAndLoadingNoCorrectDef() throws IOException {
@@ -722,6 +724,6 @@ public abstract class SamplerAspectTest {
 
         // THEN
         assertEquals(VALUE_A, getTestService().echoParameter(VALUE_B));
-
     }
+
 }
