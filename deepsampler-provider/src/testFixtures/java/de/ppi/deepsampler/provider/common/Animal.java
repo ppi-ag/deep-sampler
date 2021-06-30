34c07ff0f40e41975b57a039b6fdb94360b372fa
JanSchankin <jas@treeno.de>
2021-06-30 20:25:15 +0200

    +Test first: New Testcase for Issue #70

---
+++ deepsampler-provider/src/testFixtures/java/de/ppi/deepsampler/provider/common/Animal.java
@@ -0,0 +1,6 @@
+package de.ppi.deepsampler.provider.common;
+
+public interface Animal {
+
+    String getName();
+}
--- deepsampler-provider/src/testFixtures/java/de/ppi/deepsampler/provider/common/TestService.java
+++ deepsampler-provider/src/testFixtures/java/de/ppi/deepsampler/provider/common/TestService.java
@@ -181,4 +181,8 @@ public class TestService {
 
         return map;
     }
+
+    public Animal getAnimal() {
+        return new Dog("Porthos");
+    }
 }
