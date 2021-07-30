966ce9fde8066d7de44e252ca6213f5627865eeb
JanSchankin <jas@treeno.de>
2021-07-30 18:18:01 +0200

    +javadoc

--- deepsampler-persistence/src/main/java/de/ppi/deepsampler/persistence/error/NoMatchingSamplerFoundException.java
+++ deepsampler-persistence/src/main/java/de/ppi/deepsampler/persistence/error/NoMatchingSamplerFoundException.java
@@ -7,11 +7,11 @@ import java.util.stream.Collectors;
  * The {@link NoMatchingSamplerFoundException} is thrown if a JSON file (or any other source of persistent Samples)
  * contains Samples that don't have a matching Sampler. A Sampler is defined using {@link de.ppi.deepsampler.core.api.Sample#of(Object)}.
  *
- * A typical cause for this Exception is, that the signature of a sampled method has changed. Maybe the method name was refactored.
- * Since the complete method signature is saved in the JSON (or any other persistent medium) by default, the signature in the file doesn't
- * correspond to the refactored on in the code.
+ * A typical cause of this Exception is, that the signature of a sampled method has changed. Maybe the method name was refactored.
+ * Since the complete method signature is saved in the JSON (or any other persistent medium) by default, the signature in the file might
+ * not correspond to the refactored one in the code anymore.
  *
- * You can prevent situations like this by defining the id of a Sample manually using Sampler.of().hasId("MyId").
+ * You can prevent situations like this, by defining the id of a Sample manually using Sampler.of().hasId("MyId").
  * See {@link de.ppi.deepsampler.core.api.SampleBuilder#hasId(String)}.
  */
 public class NoMatchingSamplerFoundException extends PersistenceException {
