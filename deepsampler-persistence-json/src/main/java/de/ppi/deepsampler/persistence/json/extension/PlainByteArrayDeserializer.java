5acd81b950fe44782ca66d6e4b8743c8069a2fca
JanSchankin <jas@treeno.de>
2021-10-22 23:06:05 +0200

    Copyright cleanup

--- deepsampler-persistence-json/src/main/java/de/ppi/deepsampler/persistence/json/extension/PlainByteArrayDeserializer.java
+++ deepsampler-persistence-json/src/main/java/de/ppi/deepsampler/persistence/json/extension/PlainByteArrayDeserializer.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2021  PPI AG (Hamburg, Germany)
+ * This program is made available under the terms of the MIT License.
+ */
+
 package de.ppi.deepsampler.persistence.json.extension;
 
 import com.fasterxml.jackson.core.JsonParser;
