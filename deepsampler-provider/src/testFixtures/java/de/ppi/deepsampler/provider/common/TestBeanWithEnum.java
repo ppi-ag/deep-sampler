7e3a690a02c70e615631a2a6f2b5a2cf62018bbb
JanSchankin <jan.schankin@treeno.de>
2021-12-08 21:34:49 +0100

    🔬 Tests for enums

---
+++ deepsampler-provider/src/testFixtures/java/de/ppi/deepsampler/provider/common/TestBeanWithEnum.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright 2021  PPI AG (Hamburg, Germany)
+ * This program is made available under the terms of the MIT License.
+ */
+
+package de.ppi.deepsampler.provider.common;
+
+public class TestBeanWithEnum {
+
+    private final Ship ship;
+
+    public TestBeanWithEnum(Ship ship) {
+        this.ship = ship;
+    }
+
+    public Ship getShip() {
+        return ship;
+    }
+}
